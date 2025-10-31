6f46e454692ec3f4dd64a32f07d9d11dc6f892d2
Diego <eliwandejp@gmail.com>
2025-10-31 17:51:56 -0300

    Fix - better treatment to Special Move - Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,7 +6,6 @@ import boardgame.Position;
 import chess.exceptions.ChessException;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +13,7 @@ public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
-    private Board board;
+    private final Board board;
     private boolean check;
     private boolean checkMate;
     private ChessPiece enPassantVulnerable;
@@ -169,7 +168,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type .equals("B") && !type .equals("N") && !type .equals("R") && !type .equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
